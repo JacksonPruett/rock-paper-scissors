d3ec2809d7eb6b0b66c2e7298205e67f1914164a
Jackson Pruett <jacksonpruett@Jacksons-MBP.lan>
2024-02-21 17:49:51 -0500

    updated structure of logic

--- script.js
+++ script.js
@@ -8,27 +8,36 @@ const getComputerChoice = () => {
   return choices[randomIndex];
 };
 // console.log(getComputerChoice(1))
+
+
 const playGame = () => {
-    const gameCount = []
-    // create a function called singleRound with parameters playerSelection and computerSelection
-    const singleRound = (playerSelection, computerSelection) => {
-    //   var choices = ["Rock", "Paper", "Scissors"];
+  var choices = ["Rock", "Paper", "Scissors"];
+  const gameCount = [];
+  const playerInput = input("Please enture your choice:").toLowerCase();
+  const computerInput = choices[Math.floor(Math.random() * choices.length)];
+  // create a function called singleRound with parameters playerSelection and computerSelection
+  const singleRound = (playerSelection, computerSelection) => {
     //   for (let i = 0; i < choices.length; i++) {
-        if (playerSelection == "Rock" && computerSelection == "Scissors") {
-          return "Player wins! Rock beats Scissors!";
-        } else if (playerSelection == "Scissors" && computerSelection == "Rock") {
-          return "Computer wins! Rock beats Scissors!";
-        } else if (playerSelection == "Paper" && computerSelection == "Rock") {
-          return "Player wins! Paper beats Rock!";
-        } else if (playerSelection == "Rock" && computerSelection == "Paper") {
-          return "Computer wins! Paper beats Rock!";
-        } else if (playerSelection == "Scissors" && computerSelection == "Paper") {
-          return "Player wins! Scissors beats Paper";
-        } else playerSelection == "Paper" && computerSelection == "Scissors";
-        return "Computer wins! Scissors beats paper!";
-      }
-    };
-    console.log(singleRound("Rock", "Scissors"));
+    if (playerSelection == "Rock" && computerSelection == "Scissors") {
+      return "Player wins! Rock beats Scissors!";
+    } else if (playerSelection == "Scissors" && computerSelection == "Rock") {
+      return "Computer wins! Rock beats Scissors!";
+    } else if (playerSelection == "Paper" && computerSelection == "Rock") {
+      return "Player wins! Paper beats Rock!";
+    } else if (playerSelection == "Rock" && computerSelection == "Paper") {
+      return "Computer wins! Paper beats Rock!";
+    } else if (playerSelection == "Scissors" && computerSelection == "Paper") {
+      return "Player wins! Scissors beats Paper";
+    } else if (playerSelection == "Paper" && computerSelection == "Scissors") {
+      return "Computer wins! Scissors beats paper!";
+    } else {
+      return "It's a tie!";
+    }
+  };
+  return singleRound(playerInput, computerInput)
+};
+
+console.log(playGame())
 // }
 // use string interpolation to return  "You lose! (choice) beats (choice)!" or "You win! (choice) beats (choice)!"
 // make playerSelection case insensitive so users can input choices in any format they choose
